ea9cffeae951fea5e415ccd501b47f5a9623ad16
Vitor Cavichiolli <vitorcavichiolli@gmail.com>
2023-07-29 19:51:50 -0300

    angular electron conversion

---
+++ main.js
@@ -0,0 +1,34 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true // Permite usar o Node.js no contexto da janela do Electron.
+    }
+  });
+
+  win.loadURL(`file://${__dirname}/dist/finance-web-app/index.html`); // Carrega a página Angular no Electron.
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
