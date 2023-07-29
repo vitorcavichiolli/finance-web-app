225f7682d4733bf65b8238970ea6c942a90c4aa4
Vitor Cavichiolli <vitorcavichiolli@gmail.com>
2023-07-29 20:37:28 -0300

    side nav style and electron window preferences

--- main.js
+++ main.js
@@ -5,18 +5,17 @@ let win;
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       nodeIntegration: true // Permite usar o Node.js no contexto da janela do Electron.
     }
   });
 
   win.loadURL(`file://${__dirname}/dist/finance-web-app/index.html`); // Carrega a página Angular no Electron.
-
+  win.removeMenu();
   win.on('closed', () => {
     win = null;
   });
+  win.maximize();
 }
 
 app.on('ready', createWindow);
--- src/app/utils/pages/pages.ts
+++ src/app/utils/pages/pages.ts
@@ -1,5 +1,5 @@
 export const appPages = [
     { title: 'Home', url: '/', icon: 'home' },
-    { title: 'Reports', url: '/reports', icon: 'analytics' },
+    { title: 'Reports', url: '/reports', icon: 'trending_up' },
  
 ];
