bcdb7e705b4a802cccd5cc6aec6425c24e0397c1
Vitor Cavichiolli <vitorcavichiolli@gmail.com>
2023-07-30 20:18:44 -0300

    alteração de menu fixo para toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from './utils/data-service/data.service';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,20 @@ import { DataService } from './utils/data-service/data.service';
 export class AppComponent implements OnInit {
   title = 'finance';
   movimentacoes: any[] = [];
+  isMenuOpen = false;
+  @ViewChild('drawer') drawer!: MatDrawer;
+  @ViewChild('drawerButton') drawerButton!: any; // Use o tipo apropriado para o seu botão
   selectedMovimentacao: any = null;
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
   }
 
- 
+
+  toggleDrawer() {
+    this.drawer.toggle();
+    this.isMenuOpen = this.drawer.opened;
+  }
+
+  
 }
